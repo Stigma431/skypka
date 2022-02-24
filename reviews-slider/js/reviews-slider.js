c7ab3540fc5a04f70b63691a2c848cfb73654770
Stigma431 <86232552+Stigma431@users.noreply.github.com>
2022-02-24 18:40:09 +0500

    Пк версия слайдера отзывов

---
+++ reviews-slider/js/reviews-slider.js
@@ -0,0 +1,11 @@
+$(".reviews-slider").slick({
+    dots: true,
+    arrow: true,
+    infinite: false,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    swipeToSlide: true,
+    variableWidth: true, 
+    prevArrow: "<img src='../images/arrow-left.svg' class='prev' alt='1'>",
+    nextArrow: "<img src='../images/arrow-right.svg' class='next' alt='2'>",
+});
